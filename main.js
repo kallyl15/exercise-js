8fba5fbf4b0926b4980e584de37af318c552b90e
Kallyl Machado Lima <kallylmachadolima@gmail.com>
2023-07-17 17:09:44 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+//1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+
+alert("Hello World!")
+
+//2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+
+let NumberOne = 10
+let NumberTwo = 20
+
+alert(NumberOne + NumberTwo)
+
+//3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+    
+let number = 10
+
+const verificar = typeof  number == 'number' ? "É um número" : "Não é um número"
+
+alert(verificar)
+
+//4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+
+let String = 'Sim'
+
+const ver = typeof  String == 'string' ? "É uma string" : "Não é uma string"
+
+alert(ver)
+
+//5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+
+let Bole = false
+
+const booleano = typeof  Bole == 'boolean' ? "É um booleano" : "Não é um booleano"
+
+alert(booleano)
+
+//6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+
+let One = 10
+let Two = 10
+
+alert(One - Two)
+
+//7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+
+let OneMul = 10
+let TwoMul = 10
+
+alert(OneMul * TwoMul)
+
+//8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+
+let OneDiv = 10
+let Twodiv = 10
+
+alert(OneDiv / Twodiv)
+
+//9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+
+let NumBer = 10
+
+const ImPar = NumBer % 2 == 0 ? "É um número par" : "Não é um número par"
+alert(ImPar)
+
+//10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar"
+
+let NumBerr = 11
+
+const ImParr = NumBerr % 2 != 0 ? "É um número ímpar" : "Não é um número ímpar"
+alert(ImParr)
+
+
+
